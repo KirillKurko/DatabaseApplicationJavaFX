91a1d4337f31095c599808af72df269ebe6bada6
Кирилл Курко <kirillkurko2609@gmail.com>
2020-09-19 19:33:55 +0300

    Country class implemented

--- src/application/model/Country.java
+++ src/application/model/Country.java
@@ -1,4 +1,54 @@
 package application.model;
 
 public class Country {
+
+    private int id;
+    private String name;
+    private String capital;
+    private String language;
+
+    public Country(int id, String name, String capital, String language) {
+        this.id = id;
+        this.name = name;
+        this.capital = capital;
+        this.language = language;
+    }
+
+    public Country(String name, String capital, String language) {
+        this.name = name;
+        this.capital = capital;
+        this.language = language;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
 }
